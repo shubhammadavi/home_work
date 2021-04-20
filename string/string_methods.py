a20c33b8eff3c1225fc7ef352fa9d80ba80c1eb8
shubhammadavi <shubhammadavi44@gmail.com>
2021-04-20 22:22:48 +0530

    added strings method

---
+++ string/string_methods.py
@@ -0,0 +1,31 @@
+#capitalize()
+#print("hello".capitalize())
+
+#casefold()
+#print("SHUBHAM".casefold())
+
+#center()
+# a = "Shubham"
+# print(a.center(50))
+
+#count()
+# a = "shubham"
+# print(a.count("h"))
+
+#encode()
+# a = "my name is shubham"
+# x = a.encode()
+# print(x)
+
+#endswith()
+# a = "shubham"
+# print(a.endswith("s", 5, 7))
+
+#expandtab()
+# a = "B\tM\tW"
+# print(a.expandtabs())
+# print(a.expandtabs(2))
+# print(a.expandtabs(4))
+# print(a.expandtabs(10))
+
+#
